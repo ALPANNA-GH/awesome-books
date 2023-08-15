e8cea310e1be2aafe3c3ac01f981f68fec007c39
MarkYegon7 <markyegonk@gmail.com>
2023-08-15 19:15:50 +0300

    Fix linter errors in js file

--- script/main.js
+++ script/main.js
@@ -1,21 +1,21 @@
-const bookList = document.querySelector("#bookList");
-const bookTitle = document.querySelector("#bookTitle");
-const bookAuthor = document.querySelector("#authorName");
-const addBook = document.querySelector("#addBook");
-let bookCollection = JSON.parse(localStorage.getItem("booklist")) ?? [];
+const bookList = document.querySelector('#bookList');
+const bookTitle = document.querySelector('#bookTitle');
+const bookAuthor = document.querySelector('#authorName');
+const addBook = document.querySelector('#addBook');
+const bookCollection = JSON.parse(localStorage.getItem('booklist')) ?? [];
 
 function displayBook() {
-  bookList.innerHTML = "";
+  bookList.innerHTML = '';
   bookCollection.forEach((book) => {
-    const p1 = document.createElement("p");
+    const p1 = document.createElement('p');
     p1.textContent = `${book.title}`;
-    const p2 = document.createElement("p");
+    const p2 = document.createElement('p');
     p2.textContent = `${book.author}`;
-    let indexOfBook = document.createElement('p');
+    const indexOfBook = document.createElement('p');
     indexOfBook.id = `bookindex${book.id}`;
     indexOfBook.textContent = book.id;
-    const removeButton =  document.createElement('button');
-    removeButton.textContent = "Remove";
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
     removeButton.classList.add('deleteBtn');
     const hr = document.createElement('hr');
     bookList.append(p1, p2, indexOfBook, removeButton, hr);
@@ -24,9 +24,29 @@ function displayBook() {
   assignEventListenerToRemoveBtn();
 }
 
+function removeBook() {
+  const getId = this.previousSibling.textContent;
+  const indexToRemove = bookCollection.findIndex((book) => book.id === Number(getId));
+
+  if (indexToRemove !== -1) {
+    bookCollection.splice(indexToRemove, 1);
+    localStorage.setItem('booklist', JSON.stringify(bookCollection));
+    displayBook();
+  }
+}
+
+function assignEventListenerToRemoveBtn() {
+  const removeBtn = document.querySelectorAll('.deleteBtn');
+  removeBtn.forEach((elem) => {
+    elem.addEventListener('click', removeBook);
+  });
+}
+
 function addBookToCollection(e) {
   e.preventDefault();
-  let bookMaxId = isFinite(Math.max(...bookCollection.map(book => book.id))) ? Math.max(...bookCollection.map(book => book.id)) + 1 : 0;
+  const bookMaxId = Number.isFinite(Math.max(...bookCollection.map((book) => book.id)))
+    ? Math.max(...bookCollection.map((book) => book.id)) + 1
+    : 0;
   const book = {
     title: bookTitle.value,
     author: bookAuthor.value,
@@ -37,23 +57,5 @@ function addBookToCollection(e) {
   displayBook();
 }
 
-function removeBook() {
-  let getId = this.previousSibling.textContent;
-  let indexToRemove = bookCollection.findIndex(book => book.id === Number(getId));
-
-  if (indexToRemove !== -1) {
-    bookCollection.splice(indexToRemove, 1);
-    localStorage.setItem('booklist', JSON.stringify(bookCollection));
-    displayBook();
-  }
-}
-
-function assignEventListenerToRemoveBtn() {
-  const removeBtn = document.querySelectorAll(".deleteBtn");
-  removeBtn.forEach((elem) => {
-    elem.addEventListener("click", removeBook);
-  });
-}
-
 displayBook();
-addBook.addEventListener("click", addBookToCollection);
+addBook.addEventListener('click', addBookToCollection);
