488f233210afc26618458893ada35052bffec0c9
IsraMarvin <marvinisra08@gmail.com>
2023-08-15 20:26:29 +0500

    Fix removeBtn error

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ const bookList = document.querySelector("#bookList");
 const bookTitle = document.querySelector("#bookTitle");
 const bookAuthor = document.querySelector("#authorName");
 const addBook = document.querySelector("#addBook");
-let bookCollection = JSON.parse(localStorage.getItem("bookCollection")) ?? [];
+let bookCollection = JSON.parse(localStorage.getItem("booklist")) ?? [];
 
 function displayBook() {
   bookList.innerHTML = "";
@@ -13,15 +13,15 @@ function displayBook() {
     p2.textContent = `${book.author}`;
     let indexOfBook = document.createElement('p');
     indexOfBook.id = `bookindex${book.id}`;
-    // indexOfBook.css('obacity', 0);
     indexOfBook.textContent = book.id;
     const removeButton =  document.createElement('button');
     removeButton.textContent = "Remove";
     removeButton.classList.add('deleteBtn');
     const hr = document.createElement('hr');
     bookList.append(p1, p2, indexOfBook, removeButton, hr);
-    assignEventListenerToRemoveBtn();
   });
+
+  assignEventListenerToRemoveBtn();
 }
 
 function addBookToCollection(e) {
@@ -37,16 +37,19 @@ function addBookToCollection(e) {
   displayBook();
 }
 
-function removeBook (){
-  let getId = (this).previousSibling.textContent;
-  // alert(getId + ' ' + bookCollection.find(book => book.id === Number(getId)).title);
-  bookCollection.splice(bookCollection.find(book => book.id === Number(getId)).id, 1);
-  localStorage.setItem('booklist', JSON.stringify(bookCollection));
-  displayBook();
+function removeBook() {
+  let getId = this.previousSibling.textContent;
+  let indexToRemove = bookCollection.findIndex(book => book.id === Number(getId));
+
+  if (indexToRemove !== -1) {
+    bookCollection.splice(indexToRemove, 1);
+    localStorage.setItem('booklist', JSON.stringify(bookCollection));
+    displayBook();
+  }
 }
 
-function assignEventListenerToRemoveBtn () {
-const removeBtn = document.querySelectorAll(".deleteBtn");
+function assignEventListenerToRemoveBtn() {
+  const removeBtn = document.querySelectorAll(".deleteBtn");
   removeBtn.forEach((elem) => {
     elem.addEventListener("click", removeBook);
   });
