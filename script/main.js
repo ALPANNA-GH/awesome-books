2f2cf824081cae89b3209107501836c8008c1973
Ahmed Alpanna <alpanna@gmail.com>
2023-08-17 10:32:55 +0200

    add well formated live date & time

--- script/main.js
+++ script/main.js
@@ -135,3 +135,36 @@ contact.addEventListener('click', function() {
   heading.textContent = 'Contact Information';
   heading.style.margin = '4rem 0'
 })
+
+function showLiveTime() {
+const displayDateTime = document.getElementById('dateTime');
+let dateTime = new Date();
+let monthLong = dateTime.toLocaleString('default', {month: 'long'});
+let dayWithSuffix = dateTime.getDay();
+switch (dayWithSuffix) {
+  case 1 : 
+    dayWithSuffix += '<sup>st</sup>';
+  break;
+  case 2 :
+    dayWithSuffix += '<sup>nd</sup>';
+  break;
+  case 3 : 
+    dayWithSuffix += '<sup>rd</sup>';
+  break;
+  default : 
+    dayWithSuffix += '<sup>th</sup>';
+  break;
+}
+
+let fullYear = dateTime.getFullYear();
+let fullTimeWithSuffix = dateTime.getHours() + ':' 
+  + dateTime.getMinutes() + ':' 
+  + dateTime.getSeconds()
+  + (dateTime.getHours() > 12 ? ' <sub>pm</sub>' : ' <sub>am</sub>');
+let formatedDateTime = monthLong + ' ' + dayWithSuffix + ' ' + fullYear + ' ' + fullTimeWithSuffix;
+
+displayDateTime.innerHTML = formatedDateTime;
+setTimeout(showLiveTime, 1000);
+}
+
+showLiveTime();
